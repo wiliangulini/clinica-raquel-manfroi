d7e610e63885b996cbc62ea7c4ec23ae7fc1fe7a
wiliangulini <tochacannabis@gmail.com>
2021-01-14 11:37:26 -0300

    adicionando todo projeto da clinica raquel manfroi com paginas de menu novas

---
+++ includes/header-esp.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home | Raquel Manfroi Clínica de Fisioterapia Especializada</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,user-scalable=0" />
+        <meta name="description" content="Integramos todas as especialidades da Fisioterapia em um único ambiente, com uma equipe preparada para atender pessoas de todas as idades, preparada para atender você!">
+        <meta name="keywords" content="fisioterapia em pato branco, clinica de fisioterapia pato branco, clinica em pato branco, clinica pato branco, clinica raquel manfroi, clinica raquel manfroi em pato branco, fisioterapia uroginecológica, fisioterapia uroginecológica em pato branco, acupuntura, acupuntura em pato branco, psicologia, psicologia em pato branco, hidroginastica, hidroginastica em pato branco, pilates em pato branco, osteopatia, osteopatia em pato branco, nutricionista, nutricionista em pato branco, RPG - método fisioterapêutico em pato branco, dermato funcional, dermato funcional em pato branco, psicóloga em pato branco, psicóloga, fonoaudiologia, fonoaudiologia em pato branco, hidroterapia, hidroterapia em pato branco, natação, natação em pato branco">
+        <meta name="robots" content="index, follow">
+        <meta name="revisit-after" content="1 day">
+        <meta name="language" content="pt-BR">
+        <meta name="generator" content="N/A">
+        <meta name="author" content="Wilian Gulini" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style-fisio.css" />
+    </head>
+    <body>
+        <header>
+            <nav class="d-flex justify-content-between align-items-center">
+                <a href="index.php">
+                    <picture>
+                        <source type="image/webp" src="img/logotipo.webp" />
+                        <source type="image/png" src="img/logotipo.png" />
+                        <img src="img/logotipo.png" />
+                    </picture>
+                </a>
+                <div class="col-sm-8 col-md-8 d-flex justify-content-end">
+                    <a href="index.php">Home</a>
+                    <a href="index-sobre.php">Sobre</a>
+                    <div class="dropdown show d-flex">
+                        <a class="dropdown-toggle drop" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           Especialidades
+                        </a>
+                        <div class="dropdown-menu backitem" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="indexfisio.php">Uroginecológica</a>
+                            <a class="dropdown-item" href="indexrpg.php">RPG</a>
+                            <a class="dropdown-item" href="indexderm.php">Dermato Funcional</a>
+                            <a class="dropdown-item" href="indexfono.php">Fonoaudiologia</a>
+                            <a class="dropdown-item" href="indexterapia.php">Terapia Ocupacional</a>
+                            <a class="dropdown-item" href="indexnutri.php">Nutrição</a>
+                            <a class="dropdown-item" href="indexacup.php">Acupuntura</a>
+                            <a class="dropdown-item" href="indexpila.php">Pilates</a>
+                            <a class="dropdown-item" href="indexnata.php">Natação Infantil</a>
+                            <a class="dropdown-item" href="indexhidroginastica.php">Hidroginástica</a>
+                            <a class="dropdown-item" href="indexhidro.php">Hidroterapia</a>
+                            <a class="dropdown-item" href="indexosteo.php">Osteopatia</a>
+                            <a class="dropdown-item" href="indexpsico.php">Psicologia</a>
+                        </div>
+                    </div>
+                    <a href="convenio.php">Convênios</a>
+                </div>
+            </nav>
+        </header>
